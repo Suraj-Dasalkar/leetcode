38c637db4e87930107ea95dad3415ac83f06c9f4
Suraj-Dasalkar <suraj.dasalkar.dev@gmail.com>
2026-08-16 12:08:14 +0530

    Time: 0 ms (100%), Space: 11.6 MB (84.94%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0 ;
+        int fast = 0 ;
+        while (fast < nums.size())
+        {
+            if(nums[fast]!=val)
+            { 
+             
+              nums[slow]=nums[fast];
+              slow ++ ;
+            }
+            else 
+            {
+            }
+            fast ++ ;
+            
+
+            }
+            return slow  ;
+        }
+    
+};
